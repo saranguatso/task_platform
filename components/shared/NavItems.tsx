640b37cf935e4c99c767345858f770fbea9d2bad
Serene Tso <serenetso@Saranguas-MBP.localdomain>
2024-09-11 14:25:45 +0200

    navItem sing in  removed

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -12,17 +12,16 @@ const NavItems = () => {
     <ul className='md:flex-between flex w-full flex-col items-start gap-5 md:flex-row'>
         {headerLinks.map((link) => {
             const isActive = pathname === link.route;
-
+            
             return (
                 <li
-                    key={link.route}
-                    className={
-                        isActive ? 'text-primary-500' : ''
-                    } flex-center p-medium-16  whitespace-nowrap
-                
-                >
-                    <Link href={link.route}>{link.label}</Link>
-                </li>
+                key={link.route}
+                className={`${
+                  isActive && 'text-primary-500'
+                } flex-center p-medium-16 whitespace-nowrap`}
+              >
+                <Link href={link.route}>{link.label}</Link>
+              </li>
             )
         })}
     </ul>
