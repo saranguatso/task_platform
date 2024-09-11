606d613608a408dc75021659daf5f866c55a3890
Serene Tso <serenetso@Saranguas-MBP.localdomain>
2024-09-11 15:24:44 +0200

    db library changed

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,12 +3,12 @@
 import { revalidatePath } from 'next/cache'
 
 // import { connectToDB } from '@/lib/mongodb/mongoose'
-import { connectToDatabase } from "../database"
+import { connectToDatabase } from "@/lib/database"
 import User from "@/lib/database/models/user.model"
 import Order from "@/lib/database/models/order.model"
 import Task from "@/lib/database/models/task.model"
 
-import { handleError } from "../utils"
+import { handleError } from "@/lib/utils"
 
 import { CreateUserParams, UpdateUserParams } from "@/types"
 
