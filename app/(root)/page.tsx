572a1728bc1471f322d5b9028db5aa61ad557f29
Serene Tso <serenetso@Saranguas-MBP.localdomain>
2024-10-01 13:58:14 +0200

    Removed schooling supprot text

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -23,7 +23,6 @@ export default function Home() {
             </Button>
             </div>
           </div>
-
         </div>
       </section>
 
@@ -33,8 +32,8 @@ export default function Home() {
           </h2>
 
           <div>
-            <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-4">
-              
+            <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3">
+  
               <div className="flex flex-col gap-4">
                 <div className="flex">
                   <h5 className="h5-bold">Automated document processing</h5>
@@ -50,15 +49,6 @@ export default function Home() {
                 </div>
                   <p>
                   Our team of experts will guide your employees/students through the complex bank and personal number application processes, ensuring compliance with Swedish regulations and minimizing any delays.                  
-                  </p>
-              </div>
-
-              <div className="flex flex-col gap-4">
-                <div className="flex">
-                  <h5 className="h5-bold">Schooling and education guidance</h5>
-                </div>
-                  <p>
-                  We'll provide personalized advice on the Swedish education system ranging from daycares to universities and assist with enrolling children in local schools, ensuring a seamless transition for the entire family.    
                   </p>
               </div>
 
